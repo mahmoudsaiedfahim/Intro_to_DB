6a7e3dda460dc42c2c057091e8a577d1e5fd8338
Mahmoud Saied <mahmoudsaiedfahim@gmail.com>
2024-10-25 19:59:20 +0300

    task_4

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,6 @@
 --  prints the full description of the table books from the database alx_book_store in your MySQL server.
 USE alx_book_store;
-SHOW FULL TABLES FROM alx_book_store LIKE 'Books';
+SELECT COLUMN_NAME, COLUMN_TYPE 
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
