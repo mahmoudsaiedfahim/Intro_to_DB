2458bd407c9e2fc9da1ce3f11a6195c0efa303a7
Mahmoud Saied <mahmoudsaiedfahim@gmail.com>
2024-10-25 20:13:52 +0300

    task_5

--- task_5.sql
+++ task_5.sql
@@ -4,4 +4,4 @@ INSERT INTO customers (customer_id,
 customer_name,
 email,
 address)
-VALUES (1, "Cole Baidoo", "cbaidoo@sandteck.com", "123 Happiness Ave.");
+VALUES (1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.");
