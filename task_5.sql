0f9c0f2c92fc485b0d46bcf9d8de565d5f225eb3
Mahmoud Saied <mahmoudsaiedfahim@gmail.com>
2024-10-25 20:12:03 +0300

    task_5

---
+++ task_5.sql
@@ -0,0 +1,7 @@
+--   Insert a single row in the table customer (database alx_book_store) in your MySQL server.
+USE alx_book_store;
+INSERT INTO customers (customer_id,
+customer_name,
+email,
+address)
+VALUES (1, "Cole Baidoo", "cbaidoo@sandteck.com", "123 Happiness Ave.");
